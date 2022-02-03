be99b94dbb576580952a2faf7875257309a874bc
titov-andrei <titov_andrei@mail.ru>
2022-02-03 14:08:43 +0300

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore } from "redux";
+
+const initialState = {
+  likes: 10
+};
+
+const reducer = (state = initialState, action) => {
+  console.log('reducer > ', action);
+  return state;
+}
